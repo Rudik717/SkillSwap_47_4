c58dc57fddc9362ecd64a0c94c4216aa32166dac
Svetlana Vovk <s.vovk@inbox.ru>
2026-03-27 17:30:53 +0300

    chore: fix lint errors

--- src/ui-kit/index.ts
+++ src/ui-kit/index.ts
@@ -1,7 +1,4 @@
 export * from './Icon/Icon'
 export { Logo } from './Logo/Logo'
-<<<<<<< feature/ui-kit-add-badge-component
 export { Badge } from './Badge/Badge'
-=======
 export { Avatar } from './Avatar/Avatar'
->>>>>>> develop
