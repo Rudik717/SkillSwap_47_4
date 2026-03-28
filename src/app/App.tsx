22ff6edda206ef6fb1801703f4457ef935f331d9
Evgeniya Mikhaylyuk <jmatricaria@yandex.ru>
2026-03-29 02:54:24 +0300

    fix: компонент App возвращен в исходное состояние

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,5 @@
-import { TextInput } from '@/ui-kit'
-
 import './App.css'
 
 export const App = () => {
-  return (
-    <TextInput
-      onChange={() => {}}
-      name="password"
-      label="Пароль"
-      type="password"
-      placeholder="Введите пароль"
-      icon="eye"
-      //error='Неверный пароль'
-      disabled={true}
-    />
-  )
-
-  /*<h1>Skill Swap App.tsx</h1>*/
+  return <h1>Skill Swap App.tsx</h1>
 }
