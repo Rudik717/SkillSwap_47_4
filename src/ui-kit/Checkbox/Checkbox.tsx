b34555cc695b2cd97152faf7c4935214618bc1ee
Svetlana Vovk <s.vovk@inbox.ru>
2026-03-29 23:05:35 +0300

    chore(checkbox): remove comments, add click test

--- src/ui-kit/Checkbox/Checkbox.tsx
+++ src/ui-kit/Checkbox/Checkbox.tsx
@@ -1,29 +1,25 @@
 import styles from './Checkbox.module.css'
 
-// Задача: создать компонент Checkbox с тремя состояниями
-// Вместо псевдоэлементов используем реальные SVG-иконки из Figma
+export type CheckboxState = 'unchecked' | 'checked' | 'indeterminate'
 
 interface CheckboxProps {
-  state?: 'unchecked' | 'checked' | 'indeterminate' // три состояния чекбокса
-  onPress?: () => void // обработчик нажатия
-  children: string // текст чекбокса
+  state?: CheckboxState
+  onClick?: () => void
+  children: string
 }
 
-export const Checkbox = ({ state = 'unchecked', onPress, children }: CheckboxProps) => {
+const icons = {
+  unchecked: '/src/assets/svg/checkbox-empty.svg',
+  checked: '/src/assets/svg/checkbox-done.svg',
+  indeterminate: '/src/assets/svg/checkbox-remove.svg',
+}
+
+export const Checkbox = ({ state = 'unchecked', onClick, children }: CheckboxProps) => {
   const isChecked = state === 'checked'
   const isIndeterminate = state === 'indeterminate'
 
-  // В зависимости от состояния подставляем нужную SVG-иконку
-  let iconSrc = ''
-  if (isChecked)
-    iconSrc = '/src/assets/svg/checkbox-done.svg' // галочка
-  else if (isIndeterminate)
-    iconSrc = '/src/assets/svg/checkbox-remove.svg' // минус
-  else iconSrc = '/src/assets/svg/checkbox-empty.svg' // пустой квадрат
-
   return (
     <label className={styles.label}>
-      {/* Нативный чекбокс скрыт, нужен только для состояния и onChange */}
       <input
         type="checkbox"
         className={styles.input}
@@ -31,11 +27,9 @@ export const Checkbox = ({ state = 'unchecked', onPress, children }: CheckboxPro
         ref={(el) => {
           if (el) el.indeterminate = isIndeterminate // для состояния indeterminate
         }}
-        onChange={onPress}
+        onChange={onClick}
       />
-      {/* Кастомная иконка чекбокса */}
-      <img src={iconSrc} alt="" className={styles.icon} />
-      {/* Текст чекбокса */}
+      <img src={icons[state]} alt="" className={styles.icon} />
       <span className={styles.text}>{children}</span>
     </label>
   )
