127d860fc644e89c49243dc887f4e1cdf09e0138
Svetlana Vovk <s.vovk@inbox.ru>
2026-03-30 14:26:21 +0300

    feat(icons): add all icons story

--- src/ui-kit/Icon/Icon.tsx
+++ src/ui-kit/Icon/Icon.tsx
@@ -4,6 +4,7 @@ import ArrowLeft from '@/assets/svg/arrow-left.svg?react'
 import ArrowRight from '@/assets/svg/arrow-right.svg?react'
 import ArrowSquareLeft from '@/assets/svg/arrow-square-left.svg?react'
 import ArrowSquareRight from '@/assets/svg/arrow-square-right.svg?react'
+import ArrowUp from '@/assets/svg/arrow-up.svg?react'
 import Bell from '@/assets/svg/bell.svg?react'
 import Book from '@/assets/svg/book.svg?react'
 import Briefcase from '@/assets/svg/briefcase.svg?react'
@@ -11,7 +12,6 @@ import Calendar from '@/assets/svg/calendar.svg?react'
 import CheckboxDone from '@/assets/svg/checkbox-done.svg?react'
 import CheckboxEmpty from '@/assets/svg/checkbox-empty.svg?react'
 import CheckboxRemove from '@/assets/svg/checkbox-remove.svg?react'
-import ChevronUp from '@/assets/svg/chevron-up.svg?react'
 import Clock from '@/assets/svg/clock.svg?react'
 import Count from '@/assets/svg/count.svg?react'
 import Cross from '@/assets/svg/cross.svg?react'
@@ -46,56 +46,57 @@ import Sun from '@/assets/svg/sun.svg?react'
 import UserCircle from '@/assets/svg/user-circle.svg?react'
 import User from '@/assets/svg/user.svg?react'
 
-import styles from './Icon.module.css'
+export const icons = {
+  add: Add,
+  'arrow-up': ArrowUp,
+  'arrow-down': ArrowDown,
+  'arrow-left': ArrowLeft,
+  'arrow-right': ArrowRight,
+  'arrow-square-left': ArrowSquareLeft,
+  'arrow-square-right': ArrowSquareRight,
+  bell: Bell,
+  book: Book,
+  briefcase: Briefcase,
+  calendar: Calendar,
+  'checkbox-done': CheckboxDone,
+  'checkbox-empty': CheckboxEmpty,
+  'checkbox-remove': CheckboxRemove,
+  clock: Clock,
+  count: Count,
+  cross: Cross,
+  done: Done,
+  earth: Earth,
+  edit: Edit,
+  eye: Eye,
+  'eye-slash': EyeSlash,
+  'filter-square': FilterSquare,
+  'gallery-add': GalleryAdd,
+  'gallery-edit': GalleryEdit,
+  home: Home,
+  idea: Idea,
+  like: Like,
+  'like-filled': LikeFilled,
+  list: List,
+  logout: Logout,
+  message: Message,
+  moon: Moon,
+  'more-square': MoreSquare,
+  palette: Palette,
+  'plus-circle': PlusCircle,
+  'radiobutton-active': RadioButtonActive,
+  'radiobutton-empty': RadioButtonEmpty,
+  request: Request,
+  scroll: Scroll,
+  'scroll-big': ScrollBig,
+  search: Search,
+  share: Share,
+  sort: Sort,
+  sun: Sun,
+  user: User,
+  'user-circle': UserCircle,
+}
 
-type IconName =
-  | 'arrow-down'
-  | 'arrow-right'
-  | 'bell'
-  | 'calendar'
-  | 'clock'
-  | 'cross'
-  | 'edit'
-  | 'eye'
-  | 'gallery-add'
-  | 'idea'
-  | 'like'
-  | 'logout'
-  | 'message'
-  | 'moon'
-  | 'more-square'
-  | 'request'
-  | 'search'
-  | 'share'
-  | 'sort'
-  | 'user'
-  | 'briefcase'
-  | 'earth'
-  | 'home'
-  | 'palette'
-  | 'book'
-  | 'list'
-  | 'add'
-  | 'arrow-left'
-  | 'arrow-square-left'
-  | 'arrow-square-right'
-  | 'checkbox-done'
-  | 'checkbox-empty'
-  | 'checkbox-remove'
-  | 'chevron-up'
-  | 'count'
-  | 'done'
-  | 'eye-slash'
-  | 'filter-square'
-  | 'gallery-edit'
-  | 'like-filled'
-  | 'plus-circle'
-  | 'radiobutton-active'
-  | 'radiobutton-empty'
-  | 'scroll-big'
-  | 'scroll'
-  | 'sun'
-  | 'user-circle'
+export type IconName = keyof typeof icons
 
 type Props = {
   name: IconName
@@ -103,56 +104,6 @@ type Props = {
   size?: number
   width?: number
   height?: number
-}
-
-const icons = {
-  'arrow-down': ArrowDown,
-  'arrow-right': ArrowRight,
-  bell: Bell,
-  calendar: Calendar,
-  clock: Clock,
-  cross: Cross,
-  edit: Edit,
-  eye: Eye,
-  'gallery-add': GalleryAdd,
-  idea: Idea,
-  like: Like,
-  logout: Logout,
-  message: Message,
-  moon: Moon,
-  'more-square': MoreSquare,
-  request: Request,
-  search: Search,
-  share: Share,
-  sort: Sort,
-  user: User,
-  briefcase: Briefcase,
-  earth: Earth,
-  home: Home,
-  palette: Palette,
-  book: Book,
-  list: List,
-  add: Add,
-  'arrow-left': ArrowLeft,
-  'arrow-square-left': ArrowSquareLeft,
-  'arrow-square-right': ArrowSquareRight,
-  'checkbox-done': CheckboxDone,
-  'checkbox-empty': CheckboxEmpty,
-  'checkbox-remove': CheckboxRemove,
-  'chevron-up': ChevronUp,
-  count: Count,
-  done: Done,
-  'eye-slash': EyeSlash,
-  'filter-square': FilterSquare,
-  'gallery-edit': GalleryEdit,
-  'like-filled': LikeFilled,
-  'plus-circle': PlusCircle,
-  'radiobutton-active': RadioButtonActive,
-  'radiobutton-empty': RadioButtonEmpty,
-  'scroll-big': ScrollBig,
-  scroll: Scroll,
-  sun: Sun,
-  'user-circle': UserCircle,
 }
 
 type IconParam = {
@@ -164,28 +115,27 @@ type IconParams = Partial<Record<IconName, IconParam>>
 
 const iconParams: IconParams = {
   bell: { disableFill: true },
+  book: { disableFill: true, disableStroke: true },
   briefcase: { disableFill: true, disableStroke: true },
   earth: { disableFill: true, disableStroke: true },
   home: { disableFill: true, disableStroke: true },
-  palette: { disableFill: true, disableStroke: true },
-  book: { disableFill: true, disableStroke: true },
   list: { disableFill: true, disableStroke: true },
+  palette: { disableFill: true, disableStroke: true },
+  share: { disableFill: true },
   'user-circle': { disableFill: true },
 }
 
 /** Icon component: `<Icon name="moon" size={48} color="green" />` */
 export const Icon = ({ name, color, size, width, height }: Props) => {
   const IconComponent = icons[name]
-  const { disableFill, disableStroke } = iconParams[name] ?? {}
+  const { disableFill } = iconParams[name] ?? {}
   const widthProp = width ?? size ?? 24
   const heightProp = height ?? size ?? 24
 
   return (
     <IconComponent
-      className={styles.icon}
       fill={disableFill ? 'none' : color}
       style={{ width: widthProp, height: heightProp }}
-      stroke={disableStroke ? 'none' : (color ?? undefined)}
     />
   )
 }
