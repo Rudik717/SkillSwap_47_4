f5c9b17c17ac34e61814bfcffecb865239708927
Svetlana Vovk <s.vovk@inbox.ru>
2026-03-30 21:49:09 +0300

    chore: move constants to the separate file

--- src/ui-kit/RadioGroup/RadioGroup.tsx
+++ src/ui-kit/RadioGroup/RadioGroup.tsx
@@ -2,38 +2,6 @@ import React, { useState } from 'react'
 
 import styles from './RadioGroup.module.css'
 
-/**TODO: Перенести константы в отдельный файл */
-
-export const OPTIONS_GENDER = [
-  {
-    label: 'Не имеет значения',
-    value: 'default',
-  },
-  {
-    label: 'Мужской',
-    value: 'male',
-  },
-  {
-    label: 'Женский',
-    value: 'female',
-  },
-]
-
-export const OPTIONS_ROLE = [
-  {
-    label: 'Всё',
-    value: 'default',
-  },
-  {
-    label: 'Хочу научиться',
-    value: 'want-to-learn',
-  },
-  {
-    label: 'Могу научить',
-    value: 'can-teach',
-  },
-]
-
 type OptionType = {
   label: string
   value: string
