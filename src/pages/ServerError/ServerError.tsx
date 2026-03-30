8b738151981f815518204e3955324b7184e73fcb
Svetlana Vovk <s.vovk@inbox.ru>
2026-03-30 23:01:10 +0300

    chore: remove footer and header, use Text

--- src/pages/ServerError/ServerError.tsx
+++ src/pages/ServerError/ServerError.tsx
@@ -1,10 +1,7 @@
 import error_500 from '@/assets/svg/error 500.svg'
-import { Footer } from '@/widgets'
-import { Header } from '@/widgets'
+import { Button, Text } from '@/ui-kit'
 import type { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
-
-import { Button } from '@ui-kit/index'
 
 import styles from './ServerError.module.css'
 
@@ -16,32 +13,28 @@ export const ServerError: FC = () => {
   }
 
   return (
-    <div className={styles.notFoundPage}>
-      <Header />
-      <main className={styles.main}>
-        <img src={error_500} alt="На сервере произошла ошибка" />
-        <div className={styles.containerWrap}>
-          <div className={styles.textWrap}>
-            <h2 className={styles.errorTitle}>На сервере произошла ошибка</h2>
-            <span className={styles.errorText}>
-              Попробуйте позже или вернитесь на главную страницу
-            </span>
+    <main className={styles.main}>
+      <img src={error_500} alt="На сервере произошла ошибка" />
+      <div className={styles.containerWrap}>
+        <div className={styles.textWrap}>
+          <h2 className={styles.errorTitle}>На сервере произошла ошибка</h2>
+          <Text>Попробуйте позже или вернитесь на главную страницу</Text>
+        </div>
+
+        <div className={styles.buttonContainer}>
+          <div className={styles.buttonWidth}>
+            <Button variant="secondary" onClick={handleClickButton}>
+              <span>Сообщить об ошибке</span>
+            </Button>
           </div>
-          <div className={styles.buttonContainer}>
-            <div className={styles.buttonWidth}>
-              <Button variant="secondary" onClick={handleClickButton}>
-                <span>Сообщить об ошибке</span>
-              </Button>
-            </div>
-            <div className={styles.buttonWidth}>
-              <Button variant="primary" onClick={handleClickButton}>
-                <span>На главную</span>
-              </Button>
-            </div>
+
+          <div className={styles.buttonWidth}>
+            <Button variant="primary" onClick={handleClickButton}>
+              <span>На главную</span>
+            </Button>
           </div>
         </div>
-      </main>
-      <Footer />
-    </div>
+      </div>
+    </main>
   )
 }
