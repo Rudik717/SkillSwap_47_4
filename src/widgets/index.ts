1a50a449a90721b227f41b51fcea13c1cb72782b
Алексей <IIGamerIISaf@yandex.ru>
2026-03-31 10:41:37 +0300

    feat: добавить виджет GenderFilter
    Создан виджет GenderFilter в src/widgets/GenderFilter/
    
    Добавлены файлы: GenderFilter.tsx, GenderFilter.module.css, GenderFilter.stories.ts
    
    Использует готовый компонент RadioGroup из ui-kit
    
    Параметры options, value, onChange передаются в виджет (значение value не используется внутри, так как RadioGroup не поддерживает внешнее управление — это особенность существующего компонента)
    
    Добавлен экспорт в src/widgets/index.ts
    
    Storybook с примером использования
    
    Чужие файлы не изменены, конфликтов нет

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,3 +1,4 @@
 export { Footer } from './Footer/Footer'
 export { Header } from './Header/Header'
 export { FilterChips } from './FilterChips/FilterChips'
+export { GenderFilter } from './GenderFilter/GenderFilter'
