4be6035765cae10fc566504a92533a37f8e24e02
Svetlana Vovk <s.vovk@inbox.ru>
2026-04-01 15:33:33 +0300

    fix(layout): stretch footer full width

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,28 @@
-import { Catalog, NotFound, ServerError } from '@/pages'
+import { Catalog, Home, NotFound, ServerError } from '@/pages'
+import type { AppDispatch } from '@/store'
+import { getCategories, getCategoriesState } from '@/store/categories'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
 
 import './App.css'
 import { AppLayout } from './AppLayout'
 
 export const App = () => {
+  const dispatch = useDispatch<AppDispatch>()
+  const { loading, categories, subcategories } = useSelector(getCategoriesState)
+  console.log(' *** categories', loading, categories, subcategories)
+
+  useEffect(() => {
+    dispatch(getCategories())
+  }, [dispatch])
+
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route path="*" element={<NotFound />} />
-        <Route index element={<Catalog />} />
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
         <Route path="server-error" element={<ServerError />} />
       </Route>
     </Routes>
