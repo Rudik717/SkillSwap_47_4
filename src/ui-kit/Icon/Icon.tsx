b695cb342ac3f8029dc0e03870a8da99f0222203
Darya Kiseleva <d.kissa25@mail.ru>
2026-04-02 15:14:39 +0300

    feat: add UserGallery component with Swiper

--- src/ui-kit/Icon/Icon.tsx
+++ src/ui-kit/Icon/Icon.tsx
@@ -1,7 +1,6 @@
 import Add from '@/assets/svg/add.svg?react'
 import ArrowDown from '@/assets/svg/arrow-down.svg?react'
 import ArrowLeft from '@/assets/svg/arrow-left.svg?react'
-import ArrowRight from '@/assets/svg/arrow-right.svg?react'
 import ArrowSquareLeft from '@/assets/svg/arrow-square-left.svg?react'
 import ArrowSquareRight from '@/assets/svg/arrow-square-right.svg?react'
 import ArrowUp from '@/assets/svg/arrow-up.svg?react'
@@ -25,6 +24,7 @@ import GalleryAdd from '@/assets/svg/gallery-add.svg?react'
 import GalleryEdit from '@/assets/svg/gallery-edit.svg?react'
 import Home from '@/assets/svg/home.svg?react'
 import Idea from '@/assets/svg/idea.svg?react'
+import LeftSwitch from '@/assets/svg/left-switch.svg?react'
 import LikeFilled from '@/assets/svg/like-filled.svg?react'
 import Like from '@/assets/svg/like.svg?react'
 import List from '@/assets/svg/list.svg?react'
@@ -37,6 +37,7 @@ import PlusCircle from '@/assets/svg/plus-circle.svg?react'
 import RadioButtonActive from '@/assets/svg/radiobutton-active.svg?react'
 import RadioButtonEmpty from '@/assets/svg/radiobutton-empty.svg?react'
 import Request from '@/assets/svg/request.svg?react'
+import RightSwitch from '@/assets/svg/right-switch.svg?react'
 import ScrollBig from '@/assets/svg/scroll-big.svg?react'
 import Scroll from '@/assets/svg/scroll.svg?react'
 import Search from '@/assets/svg/search.svg?react'
@@ -51,7 +52,8 @@ export const icons = {
   'arrow-up': ArrowUp,
   'arrow-down': ArrowDown,
   'arrow-left': ArrowLeft,
-  'arrow-right': ArrowRight,
+  'left-switch': LeftSwitch,
+  'right-switch': RightSwitch,
   'arrow-square-left': ArrowSquareLeft,
   'arrow-square-right': ArrowSquareRight,
   bell: Bell,
