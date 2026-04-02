037ff6fdff8010d42594bf047761427f51ff8a87
Evgeniya Mikhaylyuk <jmatricaria@yandex.ru>
2026-04-02 04:09:45 +0300

    feat: добавлен компонент ProfileMenu

---
+++ src/widgets/ProfileMenu/ProfileMenu.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { BrowserRouter } from 'react-router-dom'
+
+import { ProfileMenu, menuItems } from './ProfileMenu'
+
+const meta: Meta<typeof ProfileMenu> = {
+  title: 'Components/ProfileMenu',
+  component: ProfileMenu,
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+  tags: ['autodocs'],
+  argTypes: {
+    selectedItem: {
+      control: {
+        type: 'select',
+        options: menuItems.map((item) => item.id),
+      },
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof ProfileMenu>
+
+// Базовый вариант
+export const Default: Story = {
+  args: {
+    items: menuItems,
+    selectedItem: 'userData',
+  },
+}
---
+++ src/widgets/ProfileMenu/ProfileMenu.tsx
@@ -0,0 +1,98 @@
+import { Button, Text } from '@/ui-kit'
+import { NavLink } from 'react-router-dom'
+
+import styles from './ProfileMenu.module.css'
+
+export type IconName = 'request' | 'message' | 'like' | 'idea' | 'user'
+
+export interface ProfileMenuElement {
+  id: string
+  label: string
+  icon: IconName
+  path: string // Путь для навигации
+}
+
+interface ProfileMenuProps {
+  items: ProfileMenuElement[]
+  selectedItem: string
+}
+
+export const menuItems: ProfileMenuElement[] = [
+  {
+    id: 'request',
+    label: 'Заявки',
+    icon: 'request',
+    path: '',
+  },
+  {
+    id: 'message',
+    label: 'Мои обмены',
+    icon: 'message',
+    path: '',
+  },
+  {
+    id: 'like',
+    label: 'Избранное',
+    icon: 'like',
+    path: '/profile/favorites',
+  },
+  {
+    id: 'idea',
+    label: 'Мои навыки',
+    icon: 'idea',
+    path: '', // Заглушка — нет пути
+  },
+  {
+    id: 'userData',
+    label: 'Личные данные',
+    icon: 'user',
+    path: '/profile/user-data', // Активный путь
+  },
+]
+
+export const ProfileMenu = ({ items, selectedItem }: ProfileMenuProps) => {
+  return (
+    <nav className={styles.container}>
+      <ul className={styles.itemsList}>
+        {items.map((item) => {
+          const isActiveLink = !!item.path //Есть путь - значит активная ссылка
+          const isSelected = item.id === selectedItem
+
+          const renderMenuItem = () => {
+            if (isActiveLink) {
+              return (
+                <NavLink to={item.path} className={styles.link}>
+                  <Button
+                    className={`
+                      ${styles.sidebarButton} 
+                      ${isSelected ? styles.sidebarButton_active : styles.disabled}`}
+                    aria-label={item.label}
+                    iconLeft={item.icon}
+                  >
+                    <Text variant="Body">{item.label}</Text>
+                  </Button>
+                </NavLink>
+              )
+            } else {
+              // Для заглушек используем кнопку с отключённым состоянием
+              return (
+                <Button
+                  className={`${styles.sidebarButton} ${styles.disabled}`}
+                  aria-label={`${item.label} (в разработке)`}
+                  iconLeft={item.icon}
+                >
+                  <Text variant="Body">{item.label}</Text>
+                </Button>
+              )
+            }
+          }
+          return (
+            <li key={item.id} className={styles.item}>
+              {renderMenuItem()}
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
