62e91d12382e72d1b549c2fd77c4bb76233ad6a7
Svetlana Vovk <s.vovk@inbox.ru>
2026-04-03 21:39:11 +0300

    chore(home): split into two components

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,26 +13,32 @@ import styles from './Home.module.css'
 
 export const Home = () => {
   const isFilterActive = useSelector(isFilterActiveSelector)
-  const popularUsers = useSelector(popularUsersSelector)
-  const newUsers = useSelector(newUsersSelector)
-  const recommendedUsers = useSelector(recommendedUsersSelector)
-  const filteredUsers = useSelector(filteredUsersSelector)
 
   return (
     <div className={styles.container}>
       <FilterPanel />
-      {isFilterActive ? (
-        <UsersGrid
-          users={filteredUsers}
-          title={`Подходящие предложения: ${filteredUsers.length}`}
-        />
-      ) : (
-        <div className={styles.cards}>
-          <UsersGrid users={popularUsers} title="Популярное" />
-          <UsersGrid users={newUsers} title="Новое" />
-          <UsersGrid users={recommendedUsers} title="Рекомендуем" />
-        </div>
-      )}
+      {isFilterActive ? <WithFilters /> : <WithoutFilters />}
+    </div>
+  )
+}
+
+const WithFilters = () => {
+  const filteredUsers = useSelector(filteredUsersSelector)
+  const matchCount = filteredUsers.length
+
+  return <UsersGrid users={filteredUsers} title={`Подходящие предложения: ${matchCount}`} />
+}
+
+const WithoutFilters = () => {
+  const popularUsers = useSelector(popularUsersSelector)
+  const newUsers = useSelector(newUsersSelector)
+  const recommendedUsers = useSelector(recommendedUsersSelector)
+
+  return (
+    <div className={styles.cards}>
+      <UsersGrid users={popularUsers} title="Популярное" />
+      <UsersGrid users={newUsers} title="Новое" />
+      <UsersGrid users={recommendedUsers} title="Рекомендуем" />
     </div>
   )
 }
