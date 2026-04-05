a1e69e47abcb9e5735983673efefc5b8591e3753
Evgeniya Mikhaylyuk <jmatricaria@yandex.ru>
2026-04-05 16:01:18 +0300

    fix: исправлена верстка, чтобы не было лишних перерендеров

--- src/pages/Registration2/Registration2.tsx
+++ src/pages/Registration2/Registration2.tsx
@@ -20,107 +20,112 @@ const genderOptions: Option[] = [
 /*TODO: использовать тип RegisterDataSet */
 
 export const Registration2 = () => {
-  const Form = () => {
-    return (
-      <div className={styles.container}>
-        <div className={styles.avatarContainer}>
-          <div className={styles.avatarIcon}>
-            <Icon name="user-circle" size={54} />
-            <div className={styles.iconAddPosition}>
-              <Icon name="add" size={16} />
-            </div>
-          </div>
-          <input
-            type="file"
-            accept="image/jpeg, image/png"
-            className={styles.avatarInput}
-            onChange={() => {}}
-          />
-        </div>
-        <TextInput
-          name="name"
-          type="text"
-          label="Имя"
-          placeholder="Введите ваше имя"
-          error=""
-          value={''}
-          onChange={() => {}}
-        />
-        <div className={styles.dateGenderBlock}>
-          <div className={styles.field}>
-            <DateInput
-              label="Дата рождения"
-              placeholder="дд.мм.гггг"
-              value={null}
-              onChange={() => {}}
-            />
-          </div>
-          <div className={styles.field}>
-            <Select
-              label="Пол"
-              placeholder="Не указан"
-              value={null}
-              options={genderOptions}
-              onChange={() => {}}
-            />
-          </div>
-        </div>
-        <div className={styles.field}>
-          <Select
-            label="Город"
-            placeholder="Не указан"
-            value={null}
-            options={[]}
-            onChange={() => {}}
-          />
-        </div>
-        <div className={styles.field}>
-          <Select
-            label="Категория навыка, которому хотите научиться"
-            placeholder="Выберете категорию"
-            value={null}
-            options={[]}
-            error=""
-            onChange={() => {}}
-          />
-        </div>
-        <div className={styles.field}>
-          <Select
-            label="Подкатегория навыка, которому хотите научиться"
-            placeholder="Выберете подкатегорию"
-            value={null}
-            options={[]}
-            error=""
-            onChange={() => {}}
-          />
-        </div>
-        <div className={styles.buttons}>
-          <Button variant="secondary" children="Назад" disabled={false} onClick={() => {}} />
-          <Button variant="primary" children="Продолжить" disabled={false} onClick={() => {}} />
-        </div>
-      </div>
-    )
-  }
-  const Onboadings = () => {
-    return (
-      <div className={styles.infoBlock}>
-        <Icon name="user-info" size={300} />
-        <div className={styles.textBlock}>
-          <Text variant="H2" className={styles.header}>
-            Расскажите немного о себе
-          </Text>
-          <Text variant="Body" className={styles.textAlign}>
-            Это поможет другим людям лучше вас узнать, чтобы{'\u00A0'}выбрать для обмена
-          </Text>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <>
       <Stepper currentStep={2} />
-      <FormLayout children={<Form />} infoBlock={<Onboadings />} />
+      <FormLayout
+        children={
+          <>
+            <div className={styles.container}>
+              <div className={styles.avatarContainer}>
+                <div className={styles.avatarIcon}>
+                  <Icon name="user-circle" size={54} />
+                  <div className={styles.iconAddPosition}>
+                    <Icon name="add" size={16} />
+                  </div>
+                </div>
+                <input
+                  type="file"
+                  accept="image/jpeg, image/png"
+                  className={styles.avatarInput}
+                  onChange={() => {}}
+                />
+              </div>
+              <TextInput
+                name="name"
+                type="text"
+                label="Имя"
+                placeholder="Введите ваше имя"
+                error=""
+                value={''}
+                onChange={() => {}}
+              />
+              <div className={styles.dateGenderBlock}>
+                <div className={styles.field}>
+                  <DateInput
+                    label="Дата рождения"
+                    placeholder="дд.мм.гггг"
+                    value={null}
+                    onChange={() => {}}
+                  />
+                </div>
+                <div className={styles.field}>
+                  <Select
+                    label="Пол"
+                    placeholder="Не указан"
+                    value={null}
+                    options={genderOptions}
+                    onChange={() => {}}
+                  />
+                </div>
+              </div>
+              <div className={styles.field}>
+                <Select
+                  label="Город"
+                  placeholder="Не указан"
+                  value={null}
+                  options={[]}
+                  onChange={() => {}}
+                />
+              </div>
+              <div className={styles.field}>
+                <Select
+                  label="Категория навыка, которому хотите научиться"
+                  placeholder="Выберете категорию"
+                  value={null}
+                  options={[]}
+                  error=""
+                  onChange={() => {}}
+                />
+              </div>
+              <div className={styles.field}>
+                <Select
+                  label="Подкатегория навыка, которому хотите научиться"
+                  placeholder="Выберете подкатегорию"
+                  value={null}
+                  options={[]}
+                  error=""
+                  onChange={() => {}}
+                />
+              </div>
+              <div className={styles.buttons}>
+                <Button variant="secondary" children="Назад" disabled={false} onClick={() => {}} />
+                <Button
+                  variant="primary"
+                  children="Продолжить"
+                  disabled={false}
+                  onClick={() => {}}
+                />
+              </div>
+            </div>
+          </>
+        }
+        infoBlock={
+          <>
+            <div className={styles.infoBlock}>
+              <Icon name="user-info" size={300} />
+              <div className={styles.textBlock}>
+                <Text variant="H2" className={styles.header}>
+                  Расскажите немного о себе
+                </Text>
+                <Text variant="Body" className={styles.textAlign}>
+                  Это поможет другим людям лучше вас узнать, чтобы{'\u00A0'}выбрать для обмена
+                </Text>
+              </div>
+            </div>
+          </>
+        }
+      />
     </>
   )
 }
