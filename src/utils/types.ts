71e8fa2c534ed6c0656b5d514d8d79d47677ae0e
Evgeniya Mikhaylyuk <jmatricaria@yandex.ru>
2026-04-07 00:03:34 +0300

    fix: изменен тип полей у TSkillData

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -82,8 +82,8 @@ export type TSkillData = {
   title?: string // необязательное поле
   description?: string
   images?: string[]
-  createdAt?: string
-  updatedAt?: string
+  createdAt: string // обязательное поле
+  updatedAt: string // обязательное поле
 }
 
 export type TRegisterData = Omit<TUserData, 'favorites' | 'likes'>
